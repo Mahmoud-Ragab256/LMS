75774c6e0c7d109c9ac666bb82d425075f50e2c7
Mahmoud Ragab <sendbad20006@gmail.com>
2026-09-08 17:28:05 +0300

    finish second section design with theme and make header fixed

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Button from "../components/ui/Button";
 import { FaArrowRight } from "react-icons/fa";
 import { FaGraduationCap } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
 import img1 from '../assets/profile-1.jpg';
 import img2 from '../assets/profile-2.jpg';
 import img3 from '../assets/profile-3.jpeg';
@@ -13,9 +14,9 @@ interface IProps {
 }
 
 function Home({ }: IProps) {
-  return (
-    <section className="m-auto p-5 py-20 flex bg-indigo-50 bg-linear-to-t from-[#FAFAFF] to-indigo-50">
-      <div className="container mx-auto space-y-7.5">
+  return (<>
+    <section className="m-auto p-5 py-25 pt-40 flex bg-indigo-50 bg-linear-to-t from-[#FAFAFF] to-indigo-50">
+      <div className="container mx-auto space-y-7.5 space-x-5 lg:space-x-10">
         <div className="p-2 px-8 bg-indigo-100 border border-indigo-400 rounded-full text-primary w-fit text-sm font-medium">
           <li>New : A-Powered Learning Path</li>
         </div>
@@ -23,10 +24,10 @@ function Home({ }: IProps) {
           <h3>Master Your Craft With</h3>
           <span className="text-primary">EDU FLOW</span>
         </div>
-        <p className="text-sm">The modern learning management system designed for clarity, focus, and measurable results. Join thousands of professionals accelerating their careers</p>
+        <p className="text-sm max-w-100">The modern learning management system designed for clarity, focus, and measurable results. Join thousands of professionals accelerating their careers</p>
         <div className="flex flex-col sm:flex-row items-center gap-2">
-          <Button className="flex items-center justify-center gap-2 hover:scale-105">Start Learning <FaArrowRight /></Button>
-          <Button className="btn-outline flex items-center justify-center gap-2 hover:scale-105 hover:bg-transparent hover:text-primary">Start Teaching <FaGraduationCap /></Button>
+          <Button className="flex items-center justify-center gap-2 hover:scale-103">Start Learning <FaArrowRight /></Button>
+          <Button className="btn-outline flex items-center justify-center gap-2 hover:scale-103 hover:bg-transparent hover:text-primary">Start Teaching <FaGraduationCap /></Button>
         </div>
         <div className="flex items-center gap-2">
           <div className="flex -space-x-3">
@@ -37,7 +38,7 @@ function Home({ }: IProps) {
           <p>Trusted by 50,000+ active learners worldwide</p>
         </div>
       </div>
-      <div className="hidden md:block container mx-auto p-10 space-y-5">
+      <div className="hidden md:block container mx-auto p-10 pt-5 space-y-5">
         <div className="flex relative gap-10">
           <div className="w-full h-100 p-5 bg-white rounded-xl flex flex-col gap-5 shadow-xl shadow-black/5">
             <img src={Physics} alt="Physics" className="rounded-lg h-[50%] object-cover" />
@@ -68,6 +69,72 @@ function Home({ }: IProps) {
         </div>
       </div>
     </section>
+    <section className="m-auto p-5 py-25 flex flex-col gap-5 items-center sm:items-start dark:text-gray-300">
+      <h4 className="text-4xl font-bold">Featured Learning Path</h4>
+      <div className="w-full flex flex-col sm:flex-row items-center justify-between gap-5">
+        <p className="text-sm max-w-100">Curated programs designed by industry experts to take you from beginner to professional in months, not years.</p>
+        <a className="flex items-center gap-2 p-2 rounded-full text-primary cursor-pointer hover:scale-103 transition duration-300" href="/courses">
+          View All Courses <FaArrowRightLong />
+        </a>
+      </div>
+      <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="w-full rounded-xl overflow-hidden flex flex-col bg-surface-light border border-gray-300 dark:bg-surface-dark dark:border-gray-700 hover:-translate-y-1 transition duration-300 shadow-md shadow-black/5 dark:shadow-white/5">
+          <a href="/courses/id"><img src={Physics} alt="Physics" className="w-full" /></a>
+          <div className="p-4">
+            <h5 className="text-lg font-medium">Featured Learning Path</h5>
+            <p className="text-sm">Curated programs designed by industry experts to take you from beginner</p>
+          </div>
+          <hr className="text-gray-100 dark:text-gray-700" />
+          <div className="p-4 flex items-center justify-between text-md">
+            <span className="block">$300</span>
+            <div className="flex items-center gap-2">
+              <h6>Mahmoud Ragab</h6>
+              <span className="block w-10 h-10 rounded-full overflow-hidden">
+                <img src={img1} alt="teacher" className="w-full h-full object-cover" />
+              </span>
+            </div>
+
+          </div>
+        </div>
+        <div className="w-full rounded-xl overflow-hidden flex flex-col bg-surface-light border border-gray-300 dark:bg-surface-dark dark:border-gray-700 hover:-translate-y-1 transition duration-300 shadow-lg shadow-black/5 dark:shadow-md dark:shadow-white/5">
+          <a href="/courses/id"><img src={Physics} alt="Physics" className="w-full" /></a>
+          <div className="p-4">
+            <h5 className="text-lg font-medium">Featured Learning Path</h5>
+            <p className="text-sm">Curated programs designed by industry experts to take you from beginner</p>
+          </div>
+          <hr className="text-gray-100 dark:text-gray-700" />
+          <div className="p-4 flex items-center justify-between text-md">
+            <span className="block">$300</span>
+            <div className="flex items-center gap-2">
+              <h6>Mahmoud Ragab</h6>
+              <span className="block w-10 h-10 rounded-full overflow-hidden">
+                <img src={img1} alt="teacher" className="w-full h-full object-cover" />
+              </span>
+            </div>
+
+          </div>
+        </div>
+        <div className="w-full rounded-xl overflow-hidden flex flex-col bg-surface-light border border-gray-300 dark:bg-surface-dark dark:border-gray-700 hover:-translate-y-1 transition duration-300 shadow-lg shadow-black/5 dark:shadow-md dark:shadow-white/5">
+          <a href="/courses/id"><img src={Physics} alt="Physics" className="w-full" /></a>
+          <div className="p-4">
+            <h5 className="text-lg font-medium">Featured Learning Path</h5>
+            <p className="text-sm">Curated programs designed by industry experts to take you from beginner</p>
+          </div>
+          <hr className="text-gray-100 dark:text-gray-700" />
+          <div className="p-4 flex items-center justify-between text-md">
+            <span className="block">$300</span>
+            <div className="flex items-center gap-2">
+              <h6>Mahmoud Ragab</h6>
+              <span className="block w-10 h-10 rounded-full overflow-hidden">
+                <img src={img1} alt="teacher" className="w-full h-full object-cover" />
+              </span>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  </>
   )
 }
 
