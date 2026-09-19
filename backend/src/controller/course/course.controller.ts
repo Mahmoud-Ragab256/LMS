600332ef9db682d9242c4ab89e33095dc0f93e69
Mahmoud Ragab <sendbad20006@gmail.com>
2026-09-19 15:14:07 +0300

    add getting methods and create courses

--- backend/src/controller/course/course.controller.ts
+++ backend/src/controller/course/course.controller.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express"
 import type { ApiResponse } from "../../types/index.js"
-import type { ICourseFilter, ICourseRes } from "../../interfaces/index.js"
-import { getAllCourses } from "../../model/pg/courseModel.js"
+import type { ICourse, ICourseFilter, ICourseRes, ICreateCourse } from "../../interfaces/index.js"
+import { createCourse, getAllCourses, getCourseById, getTeacherCourses } from "../../model/pg/courseModel.js"
 import AppError from "../../utils/appError.js"
 
 export const getCourses = async (
@@ -21,6 +21,102 @@ export const getCourses = async (
       status: 'success',
       data: courses,
     });
+  } catch (error) {
+    const message = error instanceof Error ? error.message : 'Internal Server Error';
+    const statusCode = error instanceof AppError ? error.statusCode : 500;
+    throw new AppError(statusCode, message);
+  }
+}
+
+export const addCourse = async (
+  req: Request<{}, ApiResponse<ICourse>, ICreateCourse>,
+  res: Response<ApiResponse<ICourse>>,
+): Promise<Response<ApiResponse<ICourse>>> => {
+
+  try {
+
+    const { title, price, description, imgUrl } = req.body;
+
+    if (!title || !price || !description || !imgUrl) {
+      return res.status(500).json({
+        status: 'fail',
+        message: 'Bad Request'
+      });
+    }
+
+    const course = await createCourse(res.locals.user.id, { title, price, description, imgUrl });
+
+    if (!course) {
+      return res.status(500).json({
+        status: 'fail',
+        message: 'Bad Request'
+      });
+    }
+
+    return res.json({
+      status: 'success',
+      data: course
+    });
+
+  } catch (error) {
+    const message = error instanceof Error ? error.message : 'Internal Server Error';
+    const statusCode = error instanceof AppError ? error.statusCode : 500;
+    throw new AppError(statusCode, message);
+  }
+}
+
+export const getAllTeacherCourses = async (
+  req: Request<{ teacher_id: number }, ApiResponse<ICourse[]>, {}>,
+  res: Response<ApiResponse<ICourse[]>>
+): Promise<Response<ApiResponse<ICourse[]>>> => {
+
+  try {
+
+    const { teacher_id } = req.params;
+
+    const courses = await getTeacherCourses(teacher_id);
+
+    if (!courses || courses.length === 0) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'No Courses Found'
+      });
+    }
+
+    return res.json({
+      status: 'success',
+      data: courses
+    });
+  } catch (error) {
+
+    const message = error instanceof Error ? error.message : 'Internal Server Error';
+    const statusCode = error instanceof AppError ? error.statusCode : 500;
+    throw new AppError(statusCode, message);
+  }
+}
+
+export const getCourse = async (
+  req: Request<{ id: number }, ApiResponse<ICourse>, {}>,
+  res: Response<ApiResponse<ICourse>>
+): Promise<Response<ApiResponse<ICourse>>> => {
+
+  try {
+
+    const { id } = req.params;
+
+    const course = await getCourseById(id);
+
+    if (!course) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Course Not Found'
+      });
+    }
+
+    return res.json({
+      status: 'success',
+      data: course
+    })
   } catch (error) {
     const message = error instanceof Error ? error.message : 'Internal Server Error';
     const statusCode = error instanceof AppError ? error.statusCode : 500;
